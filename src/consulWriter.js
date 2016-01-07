b8f83341e0dbcd0e4bd3e0c402fe9d97efcbdbf7
Chase Jenkins <chaseajen@gmail.com>
2016-01-07 09:41:10 -0800

    Ignore services that are already registered in the catalog on any agent

--- src/consulWriter.js
+++ src/consulWriter.js
@@ -11,24 +11,30 @@ function registerServices(consulHost, consulPort, services) {
     
     // Enumerate the services
     services.forEach(function (service) {
-        // Register the service
-        consul.agent.service.register({
-            name: service.name,
-            id: service.id,
-            address: service.address,
-            port: service.port,
-            tags: ['auto-registered'],
-        }, function (err) {
-            // Handle error
+        // Check if the service already exists on any agent
+        consul.catalog.service.nodes(service.name, function (err, data) {
             if (err) throw err;
-        });
-        // // Deregister the service by id, just in case
-        // consul.agent.service.deregister({ id: service.id }, function (err) {
-        //     // Handle error
-        //     if (err) throw err;
+            var hasService = data.some(function (item) { return item.ServiceID == service.id; });
 
+            if (hasService) {
+                console.info("service " + service.id + " already registered. Ignoring.");
+                return;
+            }
+
+            // Register the service
+            console.info("Registering service " + service.id + " to Consul");
             
-        // });
+            consul.agent.service.register({
+                name: service.name,
+                id: service.id,
+                address: service.address,
+                port: service.port,
+                tags: ['auto-registered'],
+            }, function (err) {
+                // Handle error
+                if (err) throw err;
+            });
+        });
     });
 }
 
